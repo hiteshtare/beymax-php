c93dc9dd4c8c4851ad434c29cc23c2d52429c785
hiteshtare <hiteshtare@gmail.com>
2017-05-03 18:37:51 +0530

    Fetch Service Status from RFSniffer python file .

--- getstatus.php
+++ getstatus.php
@@ -1,22 +1,25 @@
 <?php
   try
   {	
-	header('Access-Control-Allow-Origin: *'); //Cross Domain AJAX.
-	error_reporting(0);	// Turn off all error reporting
-	$config = parse_ini_file('../php_config.ini'); //read config
-	$db = new PDO($config[db_path]); //open the database
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //exception mode true 
-	
-	//to get count from status
-	$sth = $db->prepare('SELECT COUNT(*) FROM status WHERE name="RFSniffer" AND isrunning=1;');	
+		include 'mylib.php'; //include user-defined mylib
+    $db = setConfig(); //mylib - set config
+
+//to get count from status
+	$sth = $db->prepare('SELECT * FROM rooms where room=1');	
+	//$sth = $db->prepare('SELECT COUNT(*) FROM status WHERE name="RFSniffer" AND isrunning=1;');	
 	$sth->execute();
 	
 	//fetch all records into result
 	$result = $sth->fetchAll();	   						
-
-	$db = NULL;	//close the database
+		
+	$rows = count($result);//rows count
 	
-	echo json_encode($result); //print result
+		if ($rows > 0) {
+		printResult(1,'Service is running.'); 
+	}
+	else{
+		printResult(0,'Service has stopped working!');
+	}
   }
 catch (PDOException $e) 
 {
